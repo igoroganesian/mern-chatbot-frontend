3d7c38000714129d19b6cbafb0c55334a207ea0e
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-27 20:11:17 -0400

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { BrowserRouter } from "react-router-dom";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "Montserrat, serif",
+    allVariants: { color: "white" },
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+);
