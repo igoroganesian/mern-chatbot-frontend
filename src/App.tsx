ded96868cb5dbb6734dff5130a6488499ee63de6
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-28 12:53:11 -0400

    header, misc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-// import './App.css'
+import Header from "./Header";
+import { Routes, Route } from 'react-router-dom';
+import NotFound from "./NotFound";
+import Signup from "./Signup";
+import Login from "./Login";
+import Home from "./Home";
+import Chat from "./Chat";
 
 function App() {
 
   return (
-    <main>Testing</main>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Chat />} />
+        <Route path="/" element={<NotFound />} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+import Logo from "./common/Logo";
+
+const Header = () => {
+    return <AppBar sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+        <Toolbar sx={{ display: "flex" }}>
+            <Logo />
+        </Toolbar>
+    </AppBar>;
+};
+
+export default Header;
