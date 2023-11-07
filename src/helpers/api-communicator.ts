e28e07bca78ec298a2ea72355bba3fdcc0982655
Igor Oganesian <igor.oganesian@gmail.com>
2023-11-07 11:26:43 -0500

    post link

--- src/helpers/api-communicator.ts
+++ src/helpers/api-communicator.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const loginUser = async (email: string, password: string) => {
-  const res = await axios.post("/user/login", { email, password });
+  const res = await axios.post("http://localhost:5173/api/v1/user/login", { email, password });
   if (res.status !== 200) {
     throw new Error("Unable to login");
   }
